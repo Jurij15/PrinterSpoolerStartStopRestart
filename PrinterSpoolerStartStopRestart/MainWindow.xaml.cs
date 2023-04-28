939b09b03ed675470918322a89bd203a4bb54d52
Jurij15 <jurij.grom1@gmail.com>
2023-04-28 17:43:48 +0200

    remaking this

--- PrinterSpoolerStartStopRestart/MainWindow.xaml.cs
+++ PrinterSpoolerStartStopRestart/MainWindow.xaml.cs
@@ -14,15 +14,16 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Diagnostics;
-using ModernWpf;
+using Wpf.Ui;
 using PrinterSpoolerStartStopRestart.PrinterSpoolerClasses;
+using Wpf.Ui.Controls;
 
 namespace PrinterSpoolerStartStopRestart
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : UiWindow
     {
         public MainWindow()
         {
